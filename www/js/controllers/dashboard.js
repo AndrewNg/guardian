96f3cc3b686130095b3c88c8d38b2d94198274f1
Francis Hinson <francis@chesscademy.com>
2015-06-13 03:13:00 -0700

    small change

--- www/js/controllers/dashboard.js
+++ www/js/controllers/dashboard.js
@@ -1,10 +1,9 @@
 angular.module('Guardian.controllers')
 
 .controller('DashboardController', function($scope, GeolocationService){
-
-  $scope.testText = "test scope";
-
   GeolocationService.watch();
+
+
 
 });
 
--- www/js/services/geolocation.js
+++ www/js/services/geolocation.js
@@ -3,20 +3,19 @@ angular.module('Guardian.services')
 .factory('GeolocationService', function(){
 
   var watchLocation = function(){
-    document.addEventListener("deviceready", function () {
-      function onSuccess(position) {
-        console.log(position.coords.latitude);
-        console.log(position.coords.longitude);
-      }
+    function onSuccess(position) {
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+    }
 
-      // onError Callback receives a PositionError object
-      function onError(error) {
-          alert('code: '    + error.code    + '\n' +
-                'message: ' + error.message + '\n');
-      }
+    // onError Callback receives a PositionError object
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
 
-      var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
-    }, false);
+    // Options: throw an error if no update is received every 30 seconds.
+    var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
   }
 
   return {
